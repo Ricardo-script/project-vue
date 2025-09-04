3a06236eb8c236a75b9bef33ec2494d5b774ab73
ricardo <ricardo.teixeira@tradeupgroup.com>
2025-09-04 17:33:22 -0300

    multiples events

--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -3,11 +3,16 @@
         <h1>Olá Vue!</h1>
         <p>Meu nome é {{ nome }} e tabalho como {{ profissao }}</p>
     </div>
+    <MultiplosEventos />
 </template>
 
 <script>
+    import MultiplosEventos from './MultiplosEventos.vue';
     export default {
         name: ' ',
+        components: {
+            MultiplosEventos
+        },
         data(){
             return{
                 nome: 'Ricardo',
